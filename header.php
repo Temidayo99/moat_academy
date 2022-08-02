7fb4d9f434c30570c13a4db8374f30d53a340261
topinsn <olaoluwa54@gmail.com>
2022-08-01 21:54:30 -0700

    cdn insertion

--- header.php
+++ header.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="shared/bootstrap/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
     <link rel="stylesheet" href="shared/project_stylesheet.css" type="text/css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js">
 </head>
 <body>
     <header>
